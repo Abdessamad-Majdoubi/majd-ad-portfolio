1561e988b393d6f5cb529535fe663bcf4d1d7184
Abdessamad-Majdoubi <94863846+Abdessamad-Majdoubi@users.noreply.github.com>
2024-01-21 18:59:11 +0100

    LayoutTemplate.jsx

--- components/LayoutTemplate.jsx
+++ components/LayoutTemplate.jsx
@@ -170,10 +170,10 @@ export default function LayoutTemplate({ children }) {
 		<>
 			<HeaderContainer>
 				<Fade top delay={100} duration={200}>
-					<Link style={{ textDecoration: "none" }} href="https://0xtter.com" passHref>
+					<Link style={{ textDecoration: "none" }} href="https://majd-ad-portfolio.vercel.app/" passHref>
 						<LogoContainer>
 							<LogoText>
-								<CaracteresLogo>&lt;</CaracteresLogo>0xtter <CaracteresLogo>/&gt;</CaracteresLogo>
+								<CaracteresLogo>&lt;</CaracteresLogo>Majd-Ad <CaracteresLogo>/&gt;</CaracteresLogo>
 							</LogoText>
 						</LogoContainer>
 					</Link>
